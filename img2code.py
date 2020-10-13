6148afa8236bd111ed3f57d65a9a2d0bd1fb78ce
Siwei Xu <xu_siwei@hoperun.com>
2020-10-13 22:52:37 +0800

    fix half display issue

--- img2code.py
+++ img2code.py
@@ -13,32 +13,34 @@ WIDTH_BYTES = int(TARGET_WIDTH/PIXEL_PER_BYTE)
 PIXEL_THRESHOUD = 128.0
 
 # 将多个灰度像素打包到一个整数中
-def PackPixels(pixels):
+def pack_pixels(pixels):
     value = 0
     for gray in pixels:
         bit = 1 if gray >= PIXEL_THRESHOUD else 0 # 二值化
         value = (value << 1) + bit # 多个二值化像素值拼接为一个字节值
     return value
 
-def ConvertFrame(frame):
-    byteArray = []
+frameCount = 0
+def convert_frame_to_bytes(frame):
+    data = []
     # count = 0 # for debug
     start = datetime.now()
     frame = cv2.resize(frame, (TARGET_WIDTH, TARGET_HEIGHT)) # 缩放
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) # 转为灰度图
+
     for r in range(TARGET_HEIGHT):
-        for c in range(WIDTH_BYTES):
-            colStart = c * WIDTH_BYTES
+        for b in range(WIDTH_BYTES):
+            colStart = b * PIXEL_PER_BYTE
             pixels = frame[r, colStart: colStart + PIXEL_PER_BYTE]
-            byte = PackPixels(pixels)
-            byteArray.append(byte)
-            # if DEBUG and byte != 0xFF and byte != 0x00:
-            #     count += 1
-            #     print(count, ':', pixels, '0x%02X' % byte)
-    end = datetime.now()
+            byte = pack_pixels(pixels)
+            data.append(byte)
     if DEBUG:
+        global frameCount
+        cv2.imwrite(os.path.join('debug', str(frameCount) + '.png'), frame)
+        frameCount += 1
+        end = datetime.now()
         print('time cost:', end - start)
-    return byteArray
+    return bytes(data)
 
 def main():
     if len(sys.argv) < 3:
@@ -54,12 +56,12 @@ def main():
         os.mkdir(outdir)
 
     frame = cv2.imread(imgPath) # 加载图片
-    byteArray = ConvertFrame(frame) # 转为目标格式的数组
+    bitmap = convert_frame_to_bytes(frame) # 转为目标格式的数组
 
     with open(codeFile, 'w+') as f: # 输出到.c文件
         f.write('const unsigned char ' + imgBase + '[] = {\n')
-        for i in range(len(byteArray)):
-            v = byteArray[i]
+        for i in range(len(bitmap)):
+            v = bitmap[i]
             sep = '\n' if (i+1) % (TARGET_WIDTH/PIXEL_PER_BYTE) == 0 else ' '
             f.write('0x%02X,%s' % (v, sep))
         f.write('};\n')
